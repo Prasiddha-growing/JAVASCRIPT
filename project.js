14bb9985530743f1f5dfd5ffae2c56f05277e524
Prasiddha Neupane <neupaneprasiddha2004@gmail.com>
2025-10-26 14:16:56 -0500

    function to add number of lines to bet in the spin

--- project.js
+++ project.js
@@ -18,7 +18,18 @@ while (true) {
     }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
-
-
+// Numbers of line for the spins and bets
+const getnumberOfLines = () =>{
+while (true) {
+    const line = prompt("Enter the number of lines you want to bet on: ");
+        const numberOfLines = parseFloat(lines);
+        
+        if(isNaN(numberOfLines) || numberOflines <= 0 || numberOfLines >3){
+            console.log("Invalid number of lines, try again. ");
+        }else{
+            return numberOfLines;
+        }
+    }
+}
+let balance = deposit();
+const numberOfLines = getnumberOfLines();
